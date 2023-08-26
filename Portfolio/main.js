713fdf9da0c3524734522a0164848d607d8e40a6
Esther Chiemerie Joshua <joshuaesther251@gmail.com>
2023-08-26 09:42:30 -0700

    Website Updated
    spelling checks in about page and edit of portfolio Page

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -56,30 +56,7 @@ const text = document.querySelector('.typing');
 
 // Portfolio page
 
-const control = document.querySelector('#control');
-const card1 = document.querySelector('#card1');
-const card2 = document.querySelector('#card2');
-const card3 = document.querySelector('#card3');
-const instruction = document.querySelector('#instruction');
 
-control.onclick = (e) => {
-  if (e.target.classList.contains('card1')) {
-    card1.style.display = 'block';
-    card2.style.display = 'none';
-    card3.style.display = 'none';
-    instruction.style.display = 'none';
-  }else if (e.target.classList.contains('card2')) {
-    card2.style.display = 'block';
-    card1.style.display = 'none';
-    card3.style.display = 'none';
-    instruction.style.display = 'none';
-  }else if (e.target.classList.contains('card3')) {
-    card3.style.display = 'block';
-    card2.style.display = 'none';
-    card1.style.display = 'none';
-    instruction.style.display = 'none';
-  }
-}
 
 
 // Contact Page
